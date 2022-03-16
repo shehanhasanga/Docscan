c3609a3cf6c2dbc1a4ff8a0e4f31725ab10fa543
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-16 08:05:15 +0530

    Initial Commit

---
+++ DocScan/DocScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocScanApp.swift
+//  DocScan
+//
+//  Created by shehan karunarathna on 2022-03-16.
+//
+
+import SwiftUI
+
+@main
+struct DocScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
